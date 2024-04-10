197eb0974c36a454bd6e9d13b680da14b69f5262
crystalyn <00crystalynthompson@gmail.com>
2024-04-10 01:48:17 -0600

    starting to work

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const canvas = document.getElementById("main");
 var ctx = canvas.getContext('2d');
 var canvasrect = canvas.getBoundingClientRect();
-let plants = [];
-let animals = [];
+let allplants = [];
+let allanimals = [];
 let spawngrass = document.getElementById('grass');
 let spawnflower = document.getElementById('flowers');
 let spawntrees = document.getElementById('trees');
@@ -12,9 +12,10 @@ let spawnsquirel = document.getElementById('squirels');
 let spawnfrog = document.getElementById('frogs');
 let spawnfox = document.getElementById('fox');
 let spawnsnake = document.getElementById('snakes');
-let spawnflies = document.getElementById('flies');  
+let spawnflies = document.getElementById('flies'); 
+let spawnchoice = 1; 
 
-class flower{
+class plants{
     constructor(){
         this. x = 10;
         this.y = 10; 
@@ -25,30 +26,70 @@ class flower{
         this.age += 1;
     }
     draw(){
-        ctx.fillstyle = this.color;
+        ctx.fillStyle = this.color;
         ctx.beginPath();
         ctx.arc(this.x, this.y, this.size, 0, Math.PI*2);
         ctx.fill();
     }
+    // reproducing that also checks that location is within the borders
+    // trees growing rather large
+    // colision detection for trees so only squirrels can go in a tree
 }
+// class for animals
 
-function spawn(l){
-    switch(l){
-        case 1: 
-            let flower1 = new flower();
-            plants.push(flower1)
+canvas.addEventListener('click', function(event){
+    newx = event.clientX - canvasrect.left + window.scrollX
+    newy = event.clientY  + window.scrollY
+    switch(spawnchoice){
+        case 1:
+            let grass1 = new plants();
+            grass1.x = newx
+            grass1.y = newy
+            grass1.color = 'green';
+            allplants.push(grass1)
         break;
+        case 2:
+            let flower1 = new plants();
+            flower1.x = newx
+            flower1.y = newy
+            allplants.push(flower1)
+        break;
+        case 3:
+            let tree1 = new plants();
+            tree1.x = newx
+            tree1.y = newy
+            tree1.color = 'brown'
+            tree1.size = 60
+            allplants.push(tree1);
+        break;
+        case 4:
+        break;
+        case 5:
+        break;
+        case 6:
+        break;
+        case 7:
+        break;
+        case 8:
+        break;
+        case 9:
+        break;
+        case 10:
+        break;
+        
     }
-}
+})
 
 function itterate(){
-    for(j=0; j< plants.length; j++){
-        plants[j].draw();
+    for(j=0; j< allplants.length; j++){
+        allplants[j].draw();
     }
 }
-
+// count time passing in seconds, 
+//give things an age to manage growth and reproduction speeds
 function animate(){
     ctx.clearRect(0, 0, canvas.clientWidth, canvas.clientHeight);
     itterate()
     requestAnimationFrame(animate); 
 }
+animate();
